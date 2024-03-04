6223692a01271bd4453468ec126ddac90dd66625
Tiago Mendes <tiago.mendes.goes@gmail.com>
2024-03-04 20:49:59 -0300

    Project create

---
+++ src/main/java/com/legacy/MarketplaceSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.legacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceSpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceSpringMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legacy/MarketplaceSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceSpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
