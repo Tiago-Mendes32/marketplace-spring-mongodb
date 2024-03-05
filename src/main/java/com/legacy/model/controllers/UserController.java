ef663eff798b662b9e725fe02980a2f5f369f93d
Tiago PC EMPRESA <tiago.mendes.goes@gmail.com>
2024-03-05 16:21:18 -0300

    Services update

--- src/main/java/com/legacy/model/controllers/UserController.java
+++ src/main/java/com/legacy/model/controllers/UserController.java
@@ -1,17 +1,20 @@
 package com.legacy.model.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.legacy.model.entities.User;
-import com.legacy.model.repositories.UserRepository;
 import com.legacy.model.services.UserService;
 
 @RestController
@@ -20,19 +23,26 @@ public class UserController {
 
 	@Autowired
 	UserService service;
-	
+
+	@PostMapping
+	public ResponseEntity<User> save(@RequestBody User obj) {
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(location).body(service.save(obj));
+	}
+
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<User>> findAll() {
 		return ResponseEntity.ok().body(service.findAll());
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(String id){
+	public ResponseEntity<User> findById(String id) {
 		return ResponseEntity.ok().body(service.findById(id));
 	}
-	
-	@PostMapping
-	public ResponseEntity<User> save(@RequestBody User obj){
-		return ResponseEntity.created(null).body(service.save(obj));
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@RequestBody User obj, @PathVariable String id) {
+		User refObj = service.findById(id);
+		return ResponseEntity.ok().body(service.update(obj, refObj));
 	}
 }
